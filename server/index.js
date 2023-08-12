37edca2e0689cf5ab915b45338d072ccd49a2139
burnedpredator <chhariavikram1@gmail.com>
2023-08-12 22:39:01 +0530

    front end added

--- server/index.js
+++ server/index.js
@@ -16,6 +16,7 @@ import postRoutes from "./routes/posts.js";
 import { verifyToken } from "./middleware/auth.js";
 import User from "./models/User.js";
 import Post from "./models/Post.js"
+import {users, posts} from "./data/index.js"
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -59,5 +60,8 @@ mongoose
   })
   .then(() => {
     app.listen(PORT, () => console.log(`Server port: ${PORT}`));
+
+    // User.insertMany(users);
+    // Post.insertMany(posts);
   })
   .catch((error) => console.log(`${error} did not connect`));
